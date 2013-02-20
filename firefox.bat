00019c7baf529acb3d0c01643a4d71ac7f5674d1
Jon Speicher <jonspeicher@vascor.com>
2013-02-20 12:10:16 -0500

    Add Firefox launcher; use it from Markdown preview

---
+++ firefox.bat
@@ -0,0 +1,2 @@
+@echo off
+c:\progra~2\mozill~1\firefox.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
--- mp.bat
+++ mp.bat
@@ -1,4 +1,4 @@
 @echo off
 set FILENAME=%TMP%\markdown-%RANDOM%.html
 perl c:\progra~2\markdown\markdown.pl %1 > %FILENAME%
-c:\progra~2\mozill~1\firefox.exe -new-window %FILENAME%
+firefox -new-window %FILENAME%
