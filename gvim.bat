47aa5e72f9ae2dfe3378348867f74c678e7bf4cd
Jon Speicher <jonspeicher@vascor.com>
2013-02-27 15:39:32 -0500

    Make gvim.bat behave, helpful for setting CWD on launch for ack.vim

--- gvim.bat
+++ gvim.bat
@@ -1,2 +1,7 @@
 @echo off
-"C:\Program Files (x86)\Vim\vim73\gvim.exe" --remote-silent %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+if [%1] == [] (
+    start c:\progra~2\vim\vim73\gvim.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+) else (
+    start c:\progra~2\vim\vim73\gvim.exe --remote-silent %1 %2 %3 %4 %5 %6 %7 %8 %9
+)
